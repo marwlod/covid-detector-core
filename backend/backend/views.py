3ef35733ec0d51500d6327e682e95ff8df6bfba9
mardzin <martin.wlod@gmail.com>
2021-06-19 08:50:37 +0200

    Avoid code duplication

--- backend/backend/views.py
+++ backend/backend/views.py
@@ -18,22 +18,24 @@ from torchvision import transforms
 
 MODEL_NAME = "resnet50_V2.pt"
 
+
 def load_model():
     model = torchvision.models.resnet50(pretrained=True)
     model.fc = torch.nn.Linear(in_features=2048, out_features=3)
     model.load_state_dict(torch.load("../" + MODEL_NAME))
     return model
 
+
 def image_to_tensor(image, resize=True):
+    mean = [0.485, 0.456, 0.406]
+    std = [0.229, 0.224, 0.225]
     if resize:
         test_transforms = transforms.Compose([transforms.Resize(224),
                                               transforms.ToTensor(),
-                                              transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                                                                   std=[0.229, 0.224, 0.225])])
+                                              transforms.Normalize(mean=mean, std=std)])
     else:
         test_transforms = transforms.Compose([transforms.ToTensor(),
-                                              transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                                                                   std=[0.229, 0.224, 0.225])])
+                                              transforms.Normalize(mean=mean, std=std)])
     image_tensor = test_transforms(image)
     return image_tensor.unsqueeze(0)
 
